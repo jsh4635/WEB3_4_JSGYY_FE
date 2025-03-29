319fcae83815b5cdf2e88c5ba4d2e5bac88a9e79
조영재 <wo78644@gmail.com>
2025-03-29 12:07:14 +0900

    first commit

---
+++ next.config.ts
@@ -0,0 +1,32 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "*.kakaocdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "*.kakaocdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "https",
+        hostname: "*.pstatic.net",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+    ],
+    contentSecurityPolicy: "default-src 'self'; img-src 'self' data: https:;",
+  },
+};
+
+export default nextConfig;
